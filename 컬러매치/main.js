1965d8e13115232fc5cb7283297f30923e7e34db
leesangkoo96 <leesangkoo96@navr.com>
2021-07-03 23:04:46 +0900

    finally

---
+++ 컬러매치/main.js
@@ -0,0 +1,64 @@
+var result={
+
+    "black_T":{
+        "img":"clothes/반팔티_검정색.png",
+    },
+}
+
+function start() {
+    $(".start").hide();
+    $(".content").show();
+    $(".img2").hide();
+    next();
+}
+
+
+function B_Tshirt() {
+
+    document.getElementById("Tshirt").src="./clothes/반팔티_검정색.png";
+}
+
+function N_Tshirt() {
+
+    document.getElementById("Tshirt").src="./clothes/반팔티_남색.png";
+}
+function Y_Tshirt() {
+
+    document.getElementById("Tshirt").src="./clothes/반팔티_노란색.png";
+}
+function G_Tshirt() {
+
+    document.getElementById("Tshirt").src="./clothes/반팔티_초록색.png";
+}
+function W_Tshirt() {
+
+    document.getElementById("Tshirt").src="./clothes/반팔티_하얀색.png";
+}
+
+function B_pants() {
+
+    document.getElementById("pants").src="./clothes/슬랙스_검정색.png";
+}
+
+function C_pants() {
+
+    document.getElementById("pants").src="./clothes/슬랙스_크림색.png";
+}
+
+function D_pants() {
+
+    document.getElementById("pants").src="./clothes/슬랙스_데님.png";
+}
+
+function B_shoes() {
+
+    document.getElementById("shoes").src="./clothes/컨버스_검정색.png";
+    document.getElementById("shoes2").src="./clothes/컨버스_검정색(2).png";
+}
+
+function C_shoes() {
+
+    document.getElementById("shoes").src="./clothes/컨버스_크림색.png";
+    document.getElementById("shoes2").src="./clothes/컨버스_크림색(2).png";
+}
+
