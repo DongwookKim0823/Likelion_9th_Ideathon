22f9e8366cb616cd59f10ce4e43822c2f0f96a10
김진규 <legohappy@naver.com>
2021-08-08 14:27:59 +0900

    my colorstyling

--- login/mysite/colormatch/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "colormatch"
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('main', views.main, name="main"),
-] 
--- login/mysite/colormatch/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render(request, "index.html")
-
-def main(request):
-    return render(request, "main.html")
